a418ddb6a16fd14957467a243192fb7e6287657e
israil <israilbutdayev@gmail.com>
2023-09-22 14:24:07 +0400

    1

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 import express from "express";
 import child_process from "child_process";
-import axios from 'axios'
-import randomstring from "randomstring"
-import SeedRandom from "seed-random";
-console.log(SeedRandom('ajshcava'))
-console.log(randomstring.generate());
-
   
 const app = express();
 const s = "b";
 console.log("1", s);
-console.log(axios.defaults)
 app.get("/", (req, res) => {
   console.log("2", s);
   child_process.execSync("git pull")
