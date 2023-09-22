e863967e4fd2e7d1c354bb87cd034734a37f9aec
israil <israilbutdayev@gmail.com>
2023-09-22 12:48:34 +0400

    ""

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const app = express();
 
 console.log('d')
 
-app.get("/", () => {
+app.get("/", (req, res) => {
   child_process.execSync("git pull && npm install && npm start");
+  res.json({mes: 'ok'})
 });
 
 app.listen(3030);
