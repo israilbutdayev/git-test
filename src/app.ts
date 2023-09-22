e7d6d36b02a203a7b49a4ab4eccd59e47c0a134a
israil <israilbutdayev@gmail.com>
2023-09-22 15:28:46 +0400

    1

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,10 @@
 import express from "express";
 import child_process from "child_process";
-import Jimp from "jimp";
 import randomstring from "randomstring";
-console.log(randomstring.generate())
-console.log((await Jimp.create(10,10)).bitmap.height)
 const app = express();
-const s = "b";
-console.log("1", s);
 app.get("/", (req, res) => {
-  console.log("2", s);
+    const s = randomstring.generate();
   child_process.execSync("git pull")
-  console.log("3", s);
   res.json({ mes: s });
 });
 
