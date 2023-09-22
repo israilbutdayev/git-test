08f563e9e771c10448c6a09f11146064be8ee934
israil <israilbutdayev@gmail.com>
2023-09-22 13:30:12 +0400

    1

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import express from "express";
 import child_process from "child_process";
 import axios from 'axios'
+import test from 'test'
 
+test('synchronous passing test', t => {
+    // This test passes because it does not throw an exception.
+    assert.strictEqual(1, 1)
+  })
+
+  
 const app = express();
 const s = "b";
 console.log("1", s);
