7883d22e4085f52c14dea4703c9c4ed0bcfea438
israil <israilbutdayev@gmail.com>
2023-09-22 13:18:54 +0400

    1

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from "express";
 import child_process from "child_process";
-import Jimp from "jimp";
+import sharp from "sharp";
 
 const app = express();
 const s = "b";
 console.log("1", s);
-console.log((await Jimp.create(10,10)).bitmap.data)
+console.log(sharp({text:"apple"}))
 app.get("/", (req, res) => {
   console.log("2", s);
   child_process.execSync("git pull && npm install && npm start")
