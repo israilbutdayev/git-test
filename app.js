1fcd010dfee7c99ab054cc2aef3c927b4d5ce32d
israil <israilbutdayev@gmail.com>
2023-09-22 12:51:28 +0400

    ""

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 import express from "express";
 import child_process from "child_process";
 const app = express();
-
-console.log("e");
+const s = 0;
+console.log(s);
 
 app.get("/", (req, res) => {
-  console.log("e");
+  console.log(s + 1);
   child_process.spawnSync("git pull && npm install && npm start");
   res.json({ mes: "ok" });
 });
