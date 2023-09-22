af63936506d5121979dd16f30074f9bdec4acad7
israil <israilbutdayev@gmail.com>
2023-09-22 15:04:22 +0400

    1

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import child_process from "child_process";
 import Jimp from "jimp";
 console.log((await Jimp.create(10,10)).bitmap.height)
 const app = express();
-const s = "d";
+const s = "e";
 console.log("1", s);
 app.get("/", (req, res) => {
   console.log("2", s);
