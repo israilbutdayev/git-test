ee9cbfcc5f7191e0fc6382097aec17fdddfdaf25
israil <israilbutdayev@gmail.com>
2023-09-22 13:15:44 +0400

    "

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import child_process from "child_process";
+import Jimp from "jimp";
+
 const app = express();
 const s = "b";
 console.log("1", s);
-
+console.log((await Jimp.create(10,10)).bitmap.data)
 app.get("/", (req, res) => {
   console.log("2", s);
-  child_process.execSync("git pull && npm install && npm start");
+  child_process.execSync("git pull")
+  child_process.execSync("npm install && npm start");
   console.log("3", s);
   res.json({ mes: "ok" });
 });
